47a2ef89150d570e66909e8853b9c5be158d36f1
Nikolay <1488nikolay1488@gmail.com>
2022-05-20 09:28:05 +0300

    Added scaffold items to web application

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,17 +1,26 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using WebApp.Data;
+using DAL;
+using Models.Users;
+using Entities.Users;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+        throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ManagementDbContext>(options =>
+    options.UseSqlServer(connectionString)
+    .UseLazyLoadingProxies());
+
+builder.Services.AddDefaultIdentity<CustomerModel>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ManagementDbContext>();;
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
