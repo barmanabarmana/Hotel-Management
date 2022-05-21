e389f2df2eb7705b2e619386cbb4e85ebb606340
Nikolay <1488nikolay1488@gmail.com>
2022-05-22 01:57:43 +0300

    Added booking with controller and view

---
+++ WebApp/Controllers/BookingCotroller.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using BLL.Interfaces;
+using DTO;
+using DTO.Files;
+using DTO.Hotels;
+using DTO.Transports;
+using DTO.User;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.Files;
+using Models.Hotels;
+using Models.Transports;
+using Models.Users;
+using WebApp.Ninject;
+
+namespace WebApp.Controllers
+{
+    public class BookingCotroller:Controller
+    {
+        private readonly ITourService _tourService;
+        private readonly IUserService _userService;
+        public BookingCotroller()
+        {
+            _userService = UIDependencyResolver<IUserService>.ResolveDependency();
+            _tourService = UIDependencyResolver<ITourService>.ResolveDependency();
+        }
+
+        IMapper TourControllerMapper = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<CustomerDTO, CustomerModel>();
+            cfg.CreateMap<CustomerModel, CustomerDTO>();
+            cfg.CreateMap<TourDTO, TourModel>();
+            cfg.CreateMap<TourModel, TourDTO>();
+            cfg.CreateMap<HotelDTO, HotelModel>();
+            cfg.CreateMap<HotelModel, HotelDTO>();
+            cfg.CreateMap<HotelRoomDTO, HotelRoomModel>();
+            cfg.CreateMap<HotelRoomModel, HotelRoomDTO>();
+            cfg.CreateMap<TransportDTO, TransportModel>();
+            cfg.CreateMap<TransportPlaceDTO, TransportPlaceModel>();
+            cfg.CreateMap<TransportModel, TransportDTO>();
+            cfg.CreateMap<TransportPlaceModel, TransportPlaceDTO>();
+            cfg.CreateMap<ImageDTO, ImageModel>();
+            cfg.CreateMap<ImageModel, ImageDTO>();
+        }).CreateMapper();
+
+        public IActionResult BookingOnline(int id)
+        {
+            var tour = _tourService.GetTour(id);
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/BookingOnlineVM.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+using Models.Users;
+
+namespace WebApp.Models
+{
+    public class BookingOnlineVM
+    {
+        public TourModel Tour { get; set; }
+        public CustomerModel Customer { get; set; }
+    }
+}
